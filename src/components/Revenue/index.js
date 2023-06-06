0e49999862b1e9c5fa5e0898e1e0c9ab1c54ba60
trade_offf <trade_offf@163.com>
2023-06-06 18:47:41 +0800

    收益率大于40%，且小于70%的收益率可视化图表

--- src/components/Revenue/index.js
+++ src/components/Revenue/index.js
@@ -2,22 +2,38 @@ import { useEffect, useState } from 'react';
 import * as echarts from 'echarts';
 import ReactECharts from 'echarts-for-react';
 import { fetchCSV, clearData } from '../../utils';
-import { ACH, C98, UNFI } from './selectData';
+import { CKB, CRV, ENJ, EOS, IMX, LIT, LUNA2, OCEAN, SNX, TOMO, XLM } from './selectData';
 
 function RevenueChart() {
-  const [ACHData, setACHData] = useState();
-  const [C98Data, setC98Data] = useState();
-  const [UNFIData, setUNFIData] = useState();
+  const [CKBData, setCKBData] = useState();
+  const [CRVData, setCRVData] = useState();
+  const [ENJData, setENJData] = useState();
+  const [EOSData, setEOSData] = useState();
+  const [IMXData, setIMXData] = useState();
+  const [LITData, setLITData] = useState();
+  const [LUNA2Data, setLUNA2Data] = useState();
+  const [OCEANData, setOCEANData] = useState();
+  const [SNXData, setSNXData] = useState();
+  const [TOMOData, setTOMOData] = useState();
+  const [XLMData, setXLMData] = useState();
 
   useEffect(() => {
-    fetchCSV(ACH).then((data) => setACHData(clearData(data)));
-    fetchCSV(C98).then((data) => setC98Data(clearData(data)));
-    fetchCSV(UNFI).then((data) => setUNFIData(clearData(data)));
+    fetchCSV(CKB).then((data) => setCKBData(clearData(data)));
+    fetchCSV(CRV).then((data) => setCRVData(clearData(data)));
+    fetchCSV(ENJ).then((data) => setENJData(clearData(data)));
+    fetchCSV(EOS).then((data) => setEOSData(clearData(data)));
+    fetchCSV(IMX).then((data) => setIMXData(clearData(data)));
+    fetchCSV(LIT).then((data) => setLITData(clearData(data)));
+    fetchCSV(LUNA2).then((data) => setLUNA2Data(clearData(data)));
+    fetchCSV(OCEAN).then((data) => setOCEANData(clearData(data)));
+    fetchCSV(SNX).then((data) => setSNXData(clearData(data)));
+    fetchCSV(TOMO).then((data) => setTOMOData(clearData(data)));
+    fetchCSV(XLM).then((data) => setXLMData(clearData(data)));
   }, []);
 
   const option = {
     title: {
-      text: '累计收益率（小时级）',
+      text: '累计收益率（4小时级）',
     },
     tooltip: {
       trigger: 'axis',
@@ -29,30 +45,70 @@ function RevenueChart() {
       containLabel: true,
     },
     legend: {
-      data: ['ACH', 'C98', 'UNFI'],
+      data: ['CKB', 'CRV', 'ENJ', 'EOS', 'IMX', 'LIT', 'LUNA2', 'OCEAN', 'SNX', 'TOMO', 'XLM'],
     },
     xAxis: {
       type: 'category',
-      data: ACHData?.dateList,
+      data: CKBData?.dateList,
     },
     yAxis: {
       type: 'value',
     },
     series: [
       {
-        name: 'ACH',
+        name: 'CKB',
         type: 'line',
-        data: ACHData?.revenueRateList,
+        data: CKBData?.revenueRateList,
       },
       {
-        name: 'C98',
+        name: 'CRV',
         type: 'line',
-        data: C98Data?.revenueRateList,
+        data: CRVData?.revenueRateList,
       },
       {
-        name: 'UNFI',
+        name: 'ENJ',
         type: 'line',
-        data: UNFIData?.revenueRateList,
+        data: ENJData?.revenueRateList,
+      },
+      {
+        name: 'EOS',
+        type: 'line',
+        data: EOSData?.revenueRateList,
+      },
+      {
+        name: 'IMX',
+        type: 'line',
+        data: IMXData?.revenueRateList,
+      },
+      {
+        name: 'LIT',
+        type: 'line',
+        data: LITData?.revenueRateList,
+      },
+      {
+        name: 'LUNA2',
+        type: 'line',
+        data: LUNA2Data?.revenueRateList,
+      },
+      {
+        name: 'OCEAN',
+        type: 'line',
+        data: OCEANData?.revenueRateList,
+      },
+      {
+        name: 'SNX',
+        type: 'line',
+        data: SNXData?.revenueRateList,
+      },
+      {
+        name: 'TOMO',
+        type: 'line',
+        data: TOMOData?.revenueRateList,
+      },
+      {
+        name: 'XLM',
+        type: 'line',
+        data: XLMData?.revenueRateList,
       },
     ],
   };
@@ -61,7 +117,7 @@ function RevenueChart() {
     <ReactECharts
       echarts={echarts}
       option={option}
-      style={{ height: '500px' }}
+      style={{ height: '600px' }}
     />
   );
 }
--- src/components/Revenue/selectData.js
+++ src/components/Revenue/selectData.js
@@ -1,5 +1,13 @@
-import ACH from '../../data/ACH-USDT.csv';
-import C98 from '../../data/C98-USDT.csv';
-import UNFI from '../../data/UNFI-USDT.csv';
+import CKB from '../../data/CKB-USDT.csv';
+import CRV from '../../data/CRV-USDT.csv';
+import ENJ from '../../data/ENJ-USDT.csv';
+import EOS from '../../data/EOS-USDT.csv';
+import IMX from '../../data/IMX-USDT.csv';
+import LIT from '../../data/LIT-USDT.csv';
+import LUNA2 from '../../data/LUNA2-USDT.csv';
+import OCEAN from '../../data/OCEAN-USDT.csv';
+import SNX from '../../data/SNX-USDT.csv';
+import TOMO from '../../data/TOMO-USDT.csv';
+import XLM from '../../data/XLM-USDT.csv';
 
-export { ACH, C98, UNFI };
+export { CKB, CRV, ENJ, EOS, IMX, LIT, LUNA2, OCEAN, SNX, TOMO, XLM };
